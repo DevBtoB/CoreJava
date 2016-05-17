aecf2b7bb8bd5c59aee0bd67d1dc45049ab7ee9e
sbreban <breban.sergiu94@gmail.com>
2016-05-18 00:22:50 +0300

    Fix

--- CoreJava/CoreJava.ml
+++ CoreJava/CoreJava.ml
@@ -4,7 +4,7 @@ open CoreJavaUtils
 
 let rec assign (id:string) (v:stackvalue) (env:varenv) : varenv =
   match env with
-    | [] -> raise (TypeError ("Assignment to unbound variable " ^ id))
+    | [] -> raise (TypeError ("unbound variable " ^ id))
     | (id1,v1) :: t -> if id = id1 then (id,v) :: t
                         else (id1,v1) :: assign id v t
 
@@ -16,7 +16,7 @@ let rec binds (id:string) (env:varenv) : bool =
 (* gets the value of the variable from the environment*)
 let rec get_value (id:string) (env:varenv) : stackvalue =
   match env with
-    | [] -> raise (TypeError ("Unbound variable: "^id))
+    | [] -> raise (TypeError ("unbound variable: "^id))
     | (id1, v)::t -> if id=id1 then v else get_value id t
 
 let rec make_list (i:int) (v:stackvalue) : stackvalue list =
@@ -26,7 +26,7 @@ let rec make_pair (lis1:string list) (lis2:stackvalue list) : varenv =
   match (lis1, lis2) with
     | ([], []) -> [] 
     | (h1::t1, h2::t2) -> (h1,h2) :: make_pair t1 t2
-    | _ -> raise (TypeError ("Mismatched formal and actual param lists"))
+    | _ -> raise (TypeError ("formal and actual param lists does not match"))
 
 let pair_with_element (lis:string list) (v:stackvalue) : varenv =
   make_pair lis (make_list (length lis) v)
@@ -39,8 +39,8 @@ let rec var_names (varlis:var_decl list) : string list =
 let get_method_in_class (id:string) (Class(_, _, _, methlis)) : method_decl =
   let rec aux methlis = 
     match methlis with
-      | [] -> raise (TypeError ("No such method: "^id))
-      | (Method(_, m, _, _, _, _) as themethod) :: t -> if id=m then themethod else aux t
+      | [] -> raise (TypeError ("no such method: " ^ id))
+      | (Method(_, m, _, _, _, _) as the_method) :: t -> if id=m then the_method else aux t
     in aux methlis
 
 let extend_heap (st:heap) (hval:heapvalue) : heap = st @ [hval]
@@ -54,24 +54,25 @@ let rec replace_nth i x lis =
   match (i, lis) with
     | (0, _::t) -> x :: t
     | (n, h::t) -> h :: replace_nth (n-1) x t
+    | (_,_) -> raise (RuntimeError "memory error")
 
-let assign_object (sto:heap) (loc:int) (obj:heapvalue) =
-  replace_nth loc obj sto;;
+let assign_object (heap:heap) (loc:int) (obj:heapvalue) =
+  replace_nth loc obj heap;;
 
 let get_class (c:string) (Program classlis) : class_decl =
   let rec aux classlis = 
     match classlis with
-      | [] -> raise (TypeError ("No such class: "^c))
-      | (Class(c1, _, _, _) as theclass) :: t -> if c=c1 then theclass else aux t
+      | [] -> raise (TypeError ("no such class: " ^ c))
+      | (Class(c1, _, _, _) as the_class) :: t -> if c=c1 then the_class else aux t
     in aux classlis
 
 let rec get_method (id:string) (c:string) (prog:program) : method_decl =
-  let rec hasMethod (id:string) (methlis: method_decl list) : bool =
+  let rec has_mathod (id:string) (methlis: method_decl list) : bool =
     match methlis with
       | [] -> false
-      | Method(_,m,_,_,_,_)::t -> id=m or hasMethod id t
+      | Method(_,m,_,_,_,_)::t -> id=m || has_mathod id t
     in let Class(_,s,_,methlis) as cls = get_class c prog
-      in if hasMethod id methlis then get_method_in_class id cls
+      in if has_mathod id methlis then get_method_in_class id cls
           else if s="" then raise (TypeError ("No such method: "^id))
             else get_method id s prog
 
@@ -91,122 +92,122 @@ let apply_operation (bop:binary_operation)
     | Plus -> (match (v1, v2) with
         | (IntV i, IntV j) -> IntV (i + j)
         | (StringV i, StringV j) -> StringV (i ^ j)
-        | _ -> raise (TypeError "Plus: Failed to convert arguments"))
+        | _ -> raise (TypeError "plus failed"))
     | Minus -> (match (v1, v2) with
         | (IntV i, IntV j) -> IntV (i - j)
-        | _ -> raise (TypeError "Minus: Failed to convert arguments"))
+        | _ -> raise (TypeError "minus failed"))
     | Multiplication -> (match (v1, v2) with
         | (IntV i, IntV j) -> IntV (i * j)
-        | _ -> raise (TypeError "Multiplication: Failed to convert arguments"))
+        | _ -> raise (TypeError "multiplication failed"))
     | Division -> (match (v1, v2) with
-        | (IntV _, IntV 0) -> raise (RuntimeError "Division by zero")
+        | (IntV _, IntV 0) -> raise (RuntimeError "division by zero")
         | (IntV i, IntV j) -> IntV (i / j)
-        | _ -> raise (TypeError "Division: Failed to convert arguments"))
+        | _ -> raise (TypeError "division failed"))
     | LessThan -> (match (v1, v2) with
         | (IntV i, IntV j) -> BoolV (i < j)
-        | _ -> raise (TypeError "LessThan: Failed to convert arguments"))
+        | _ -> raise (TypeError "less than failed"))
     | Equal -> (match (v1, v2) with
           (IntV i, IntV j) -> BoolV (i=j)
         | (BoolV b1, BoolV b2) -> BoolV (b1=b2)
         | (NullV, NullV) -> BoolV true
         | (NullV, _) -> BoolV false
         | (_, NullV) -> BoolV false
-        | _ -> raise (TypeError "Equal: Failed to convert arguments"))
-    | _ -> raise (RuntimeError "apply_operation: Operator not supported")
+        | _ -> raise (TypeError "equal failed"))
+    | _ -> raise (RuntimeError "operator not supported")
 
-let rec eval (e:exp) ((env,sto) as sigma:state) (prog:program) : stackvalue * heap =
+let rec eval (e:exp) ((env,heap) as currstate:state) (prog:program) : stackvalue * heap =
   match e with
-    | String s -> (StringV s, sto)
-    | Integer i -> (IntV i, sto)
-    | True -> (BoolV true, sto)
-    | False -> (BoolV false, sto)
-    | Null -> (NullV, sto)
+    | String s -> (StringV s, heap)
+    | Integer i -> (IntV i, heap)
+    | True -> (BoolV true, heap)
+    | False -> (BoolV false, heap)
+    | Null -> (NullV, heap)
     | Id id -> if binds id env
-               then (get_value id env, sto)
+               then (get_value id env, heap)
                else (match get_value "this" env with
-                      | Location loc -> let Object(_,flds) = get_heap_value sto loc
-                           in (get_value id flds, sto)
+                      | Location loc -> let Object(_,flds) = get_heap_value heap loc
+                           in (get_value id flds, heap)
                       | _ -> raise (TypeError ("Undefined variable: "^id)))
-    | This -> (get_value "this" env, sto)
-    | Not e1 -> let (v,sto1) = eval e1 sigma prog
+    | This -> (get_value "this" env, heap)
+    | Not e1 -> let (v,heap1) = eval e1 currstate prog
                 in (match v with
-                  | BoolV b -> (BoolV (not b), sto1)
+                  | BoolV b -> (BoolV (not b), heap1)
                   | _ -> raise (TypeError "Not applied to non-bool"))
     | Operation(e1, And, e2) ->
-          let (v,sto1) = eval e1 sigma prog
+          let (v,heap1) = eval e1 currstate prog
           in (match v with
-            | BoolV true -> eval e2 (env,sto1) prog
-            | BoolV false -> (BoolV false, sto1)
+            | BoolV true -> eval e2 (env,heap1) prog
+            | BoolV false -> (BoolV false, heap1)
             | _ -> raise (TypeError "And applied to non-boolean"))
     | Operation(e1, Or, e2) ->
-          let (v,sto1) = eval e1 sigma prog
+          let (v,heap1) = eval e1 currstate prog
           in (match v with
-            | BoolV true -> (BoolV true, sto1)
-            | BoolV false -> eval e2 (env,sto1) prog
+            | BoolV true -> (BoolV true, heap1)
+            | BoolV false -> eval e2 (env,heap1) prog
             | _ -> raise (TypeError "And applied to non-boolean"))
     | Operation(e1, bop, e2) ->
-          let (vl,sto1) = eval_list [e1; e2] sigma prog
-          in (apply_operation bop (hd vl) (hd (tl vl)), sto1)
-    | NewId c -> let newloc = List.length sto and flds = get_field_list c prog
+          let (vl,heap1) = eval_list [e1; e2] currstate prog
+          in (apply_operation bop (hd vl) (hd (tl vl)), heap1)
+    | NewId c -> let newloc = List.length heap and flds = get_field_list c prog
                   in let obj = Object(c, pair_with_element flds NullV)
-                  in (Location newloc, extend_heap sto obj)
+                  in (Location newloc, extend_heap heap obj)
     | MethodCall(e1, id, args) ->
-          let (sv,sto1) = eval e1 sigma prog 
+          let (sv,heap1) = eval e1 currstate prog 
           in (match sv with
             | (Location loc) as this ->
-              let Object(c,flds) = get_heap_value sto1 loc
+              let Object(c,flds) = get_heap_value heap1 loc
               in let Method(_, _, args, locals, stms, retval) = get_method id c prog
-                and (argvals,sto2) = eval_list args (env,sto1) prog
+                and (argvals,heap2) = eval_list args (env,heap1) prog
                   in let parambindings = make_pair (var_names args) argvals
                     and localbindings = pair_with_element (var_names locals) NullV
-                      in let env' = ("this",this) :: (parambindings @ localbindings)
-                        in eval_method_call stms retval (env',sto2) prog
-            | _ -> raise (TypeError "Receiver not an object in method call"))
-    | _ -> raise (RuntimeError "eval: Expression not supported")
+                      in let env1 = ("this",this) :: (parambindings @ localbindings)
+                        in eval_method_call stms retval (env1,heap2) prog
+            | _ -> raise (TypeError "object not found"))
+    | _ -> raise (RuntimeError "eval failed")
 
-and eval_list (el:exp list) ((env,sto) as sigma:state) (prog:program) : stackvalue list * heap = 
+and eval_list (el:exp list) ((env,heap) as currstate:state) (prog:program) : stackvalue list * heap = 
     match el with
-      | [] -> ([], sto)
-      | h::t -> let (sv,sto1) = eval h sigma prog
-          in let (svl,sto2) = eval_list t (env,sto1) prog
-            in (sv::svl, sto2)
+      | [] -> ([], heap)
+      | h::t -> let (sv,heap1) = eval h currstate prog
+          in let (svl,heap2) = eval_list t (env,heap1) prog
+            in (sv::svl, heap2)
 
-and eval_method_call (stms:statement list) (retval:exp) (sigma:state)
+and eval_method_call (stms:statement list) (retval:exp) (currstate:state)
       (prog:program) : stackvalue * heap =
-    let sigma1 = exec_statement_list stms sigma prog
-      in eval retval sigma1 prog
+    let currstate1 = exec_statement_list stms currstate prog
+      in eval retval currstate1 prog
 
-and exec_statement (s:statement) ((env,sto) as sigma:state) (prog:program) : state =
+and exec_statement (s:statement) ((env,heap) as currstate:state) (prog:program) : state =
     match s with
       | Assignment(id,e) ->
-           let (sv,sto1) = eval e (env,sto) prog
+           let (sv,heap1) = eval e (env,heap) prog
            in if binds id env
-              then (assign id sv env, sto1)
+              then (assign id sv env, heap1)
               else (match get_value "this" env with
                     | Location loc ->
-                       let obj = get_heap_value sto1 loc
-                       in (env, assign_object sto1 loc (assign_field obj id sv))
+                       let obj = get_heap_value heap1 loc
+                       in (env, assign_object heap1 loc (assign_field obj id sv))
                     | _ -> raise (TypeError("Assignment to ndefined variable: "^id)))
-      | If(e,s1,s2) -> let (sv,sto1) = eval e (env,sto) prog
+      | If(e,s1,s2) -> let (sv,heap1) = eval e (env,heap) prog
                      in (match sv with
-                        | BoolV true -> exec_statement s1 (env,sto1) prog
-                        | BoolV false -> exec_statement s2 (env,sto1) prog
+                        | BoolV true -> exec_statement s1 (env,heap1) prog
+                        | BoolV false -> exec_statement s2 (env,heap1) prog
                         | _ -> raise (TypeError "Non-bool in if stmt"))
-      | Block sl -> exec_statement_list sl sigma prog
+      | Block sl -> exec_statement_list sl currstate prog
       | _ -> raise (RuntimeError "exec_statement: Statement not supported")
 
-and exec_statement_list (sl:statement list) (sigma:state) (prog:program) : state =
+and exec_statement_list (sl:statement list) (currstate:state) (prog:program) : state =
     match sl with
-      | [] -> sigma
-      | s::slt -> let sigma1 = exec_statement s sigma prog
-                    in exec_statement_list slt sigma1 prog
+      | [] -> currstate
+      | s::slt -> let currstate1 = exec_statement s currstate prog
+                    in exec_statement_list slt currstate1 prog
 
 let run_with_args (Program(Class(cname,_,_,_) :: _) as prog)
                   (args:exp list) : string =
    let env = [("this", Location 0)]
-   and sto = [Object(cname, [])]
+   and heap = [Object(cname, [])]
    in let (v,_) = eval (MethodCall(Id "this", "main", args))
-                       (env,sto) prog
+                       (env,heap) prog
       in string_of_stackval v
 
 let run (prog:program) : string = run_with_args prog [Integer 2]
@@ -229,16 +230,16 @@ make_pair ["a"; "b"] [IntV 10; BoolV true];;
 pair_with_element ["a"; "b"] NullV;;
 var_names [Var(IntType, "i"); Var(BoolType, "isEof")];;
 
-let sto1 = [Object("C", [("x", IntV 4); ("y", BoolV true)])];;
-extend_heap sto1 (Object("D", [("a", IntV 7); ("b", Location 0)]));;
+let heap1 = [Object("C", [("x", IntV 4); ("y", BoolV true)])];;
+extend_heap heap1 (Object("D", [("a", IntV 7); ("b", Location 0)]));;
 
 let obj1 = Object("C", [("x", IntV 4); ("y", BoolV false)]);;
 assign_field obj1 "y" (Location 3);;
 
-let sto2 = [Object("C", [("x", IntV 4); ("y", BoolV true)]);
+let heap2 = [Object("C", [("x", IntV 4); ("y", BoolV true)]);
 Object("D", [("a", IntV 7); ("b", Location 0)])];;
 let obj2 = Object ("C", [("x", IntV 4); ("y", Location 3)]);;
-assign_object sto2 1 obj2;;
+assign_object heap2 1 obj2;;
 
 let p1 = Program
    [Class ("Main", "", [],
